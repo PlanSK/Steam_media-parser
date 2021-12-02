157b5adc52616c9bee2e54924c8220c1aca1dd3c
Sergey K <skapkanshchikov@gmail.com>
2021-12-02 13:13:33 +0300

    Add decorators. Add disabling gsheets write option. Game analize to function.

--- gsheets.py
+++ gsheets.py
@@ -2,15 +2,17 @@ import gspread
 import gspread_formatting
 
 from gspread_formatting.models import TextFormat
+from main import status_operation
 
 
+@status_operation(title='Google sheets read data')
 def gsheets_read(api: str, table_key: str) -> list:
     google_connect = gspread.service_account(filename=api)
     gsheet = google_connect.open_by_key(table_key)
 
     return gsheet.sheet1.col_values(1)[1:]
 
-
+@status_operation(title='Google sheets export data')
 def gsheets_save(
     api: str, table_key: str, data: dict, 
     title_colorize: bool = False) -> None:
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import requests
 import os
 import json
+from typing import Callable
 
 from bs4 import BeautifulSoup
 from requests.api import request
 import tqdm
 import gsheets
+
+
+def status_operation(title: str):
+    def get_operation(operation: Callable) -> Callable:
+        def wrapper(*args, **kwargs):
+            print(title, end='')
+            result = operation(*args, **kwargs)
+            print('...OK.')
+            return result
+        return wrapper
+    return get_operation
 
 
 def download_media(get_url: str, file_path: str) -> None:
@@ -54,9 +66,7 @@ def write_media_link(
     if os.path.exists(image_file_path):
         file_size = os.path.getsize(image_file_path)
         remote_file_size = int(requests.head(url=src_link).headers.get('content-length'))
-        if file_size == remote_file_size:
-            print(f'{image_file_path} exists - passed.')
-        else:
+        if file_size != remote_file_size:
             download_media(get_url=src_link, file_path=image_file_path)
     else:
         download_media(get_url=src_link, file_path=image_file_path)
@@ -64,17 +74,107 @@ def write_media_link(
     return image_file_name
 
 
-if __name__ == "__main__":
-    print('Esports Tatar Steam Parser (c)')
-    print('Version 2.0. All rights reserved.')
+@status_operation(title=f'Get game ')
+def game_analyze(game_id: int) -> dict:
+    game_dict = dict()
+    game_title = ''
+    game_icon = ''
+    genres = []
+    main_game_image = ''
+    short_description = ''
+    tags = []
+    media_files_list = []
 
-    settings_file = 'settings.json'
+    url_steam = f'https://store.steampowered.com/app/{game_id}/?l=russian'
+    page = requests.get(url=url_steam)
+    soup = BeautifulSoup(page.text, "lxml")
+
+    if page.status_code == 200:
+        game_title = soup.find(id='appHubAppName').string
+        print(game_title, end='')
+
+        game_folder_name = f'{game_title}-{game_id}'
+
+        if not os.path.exists(os.path.join(os.getcwd(), 'data')):
+            os.mkdir(os.path.join(os.getcwd(), 'data'))
+
+        abs_folder_path = os.path.join(os.getcwd(), 'data', game_folder_name)
+        game_data = (game_id, game_title, abs_folder_path)
+
+        if not os.path.exists(abs_folder_path):
+            os.mkdir(abs_folder_path)
+
+        main_game_image_src = soup.find(class_='game_header_image_full')['src']
+        main_game_image = write_media_link(game_data, 'main_img', main_game_image_src)
+
+        game_icon_src = soup.find(class_='apphub_AppIcon').img['src']
+        game_icon = write_media_link(game_data, 'icon', game_icon_src)
+
+        short_description = soup.find(class_='game_description_snippet').string.strip()
+
+        tags = [
+            tag.string.strip()
+            for tag in soup.find(class_='glance_tags popular_tags').find_all('a')
+        ]
+        genres = [
+            genre.string
+            for genre in soup.find(id='genresAndManufacturer').span.find_all('a')
+        ]
+        media_data = soup.find(id='highlight_player_area')
+        mp4_files = media_data.find_all('div', 'highlight_player_item highlight_movie')
+        screenshots = media_data.find_all('div', 'highlight_player_item highlight_screenshot')
+
+        media_files_list = []
+
+        if settings['image_download']:
+            for number, img_media in enumerate(screenshots, start=1):
+                media_src = img_media.div.find('a', 'highlight_screenshot_link')['href']
+                media_files_list.append(write_media_link(game_data, 'screenshot', media_src, media_counter=number))
+
+        if settings['video_download']:
+            for number, mp4_media in enumerate(mp4_files, start=1):
+                media_src = mp4_media['data-mp4-source']
+                media_files_list.append(write_media_link(game_data, 'video', media_src, media_counter=number))
+
+    else:
+        print(f'Error {page.status_code} read game id {game_id}')
+    
+    game_dict[game_id] = {
+        'title': game_title,
+        'icon': game_icon,
+        'genres': ', '.join(genres),
+        'main_img': main_game_image,
+        'tags': ', '.join(tags),
+        'description': short_description,
+        'screenshots': ', '.join(media_files_list)
+    }
+
+    return game_dict
+
+
+def load_settings(settings_file: str) -> dict:
     try:
         with open(settings_file, 'r') as json_file:
             settings = json.load(json_file)
     except FileNotFoundError:
-        print('Need the settings file for a work.')
-        raise FileNotFoundError
+        print('Settings file not found. Load default.')
+        settings = {
+            'table': '1HXP6i8m1MfofyY-nUDx8F686NME_poBrlU8nDdidYso',
+            'image_download': False,
+            'video_download': False,
+            'gsheets_data_file': 'esports_cred.json',
+            'colorize_title_gsheets': False,
+            'google_sheets_write': False
+        }
+    return settings
+
+
+if __name__ == "__main__":
+    print('Esports Tatar Steam Parser (c)')
+    print('Version 2.5. All rights reserved.')
+
+    settings_file = 'settings.json'
+    settings = load_settings(settings_file)
 
     export_data = dict()
 
@@ -83,80 +183,12 @@ if __name__ == "__main__":
         raise ValueError('Games list not defined.')
 
     for game_id in games_list:
-        game_title = ''
-        game_icon = ''
-        genres = []
-        main_game_image = ''
-        short_description = ''
-        tags = []
-        media_files_list = []
+        export_data.update(game_analyze(game_id=game_id))
 
-        url_steam = f'https://store.steampowered.com/app/{game_id}/?l=russian'
-        page = requests.get(url=url_steam)
-        soup = BeautifulSoup(page.text, "lxml")
-
-        if page.status_code == 200:
-            game_title = soup.find(id='appHubAppName').string
-            game_folder_name = f'{game_title}-{game_id}'
-
-            if not os.path.exists(os.path.join(os.getcwd(), 'data')):
-                os.mkdir(os.path.join(os.getcwd(), 'data'))
-
-            abs_folder_path = os.path.join(os.getcwd(), 'data', game_folder_name)
-            game_data = (game_id, game_title, abs_folder_path)
-
-            if not os.path.exists(abs_folder_path):
-                os.mkdir(abs_folder_path)
-
-            main_game_image_src = soup.find(class_='game_header_image_full')['src']
-            main_game_image = write_media_link(game_data, 'main_img', main_game_image_src)
-
-            game_icon_src = soup.find(class_='apphub_AppIcon').img['src']
-            game_icon = write_media_link(game_data, 'icon', game_icon_src)
-
-            short_description = soup.find(class_='game_description_snippet').string.strip()
-
-            tags = [
-                tag.string.strip()
-                for tag in soup.find(class_='glance_tags popular_tags').find_all('a')
-            ]
-            genres = [
-                genre.string
-                for genre in soup.find(id='genresAndManufacturer').span.find_all('a')
-            ]
-            media_data = soup.find(id='highlight_player_area')
-            mp4_files = media_data.find_all('div', 'highlight_player_item highlight_movie')
-            screenshots = media_data.find_all('div', 'highlight_player_item highlight_screenshot')
-
-            media_files_list = []
-
-            if settings['image_download']:
-                for number, img_media in enumerate(screenshots, start=1):
-                    media_src = img_media.div.find('a', 'highlight_screenshot_link')['href']
-                    media_files_list.append(write_media_link(game_data, 'screenshot', media_src, media_counter=number))
-
-            if settings['video_download']:
-                for number, mp4_media in enumerate(mp4_files, start=1):
-                    media_src = mp4_media['data-mp4-source']
-                    media_files_list.append(write_media_link(game_data, 'video', media_src, media_counter=number))
-
-        else:
-            print(f'Error {page.status_code} read game id {game_id}')
-
-        export_data.update({ game_id : {
-                'title': game_title,
-                'icon': game_icon,
-                'genres': ', '.join(genres),
-                'main_img': main_game_image,
-                'tags': ', '.join(tags),
-                'description': short_description,
-                'screenshots': ', '.join(media_files_list)
-                } }
-            )
-
-    gsheets.gsheets_save(
-        settings['gsheets_data_file'],
-        settings['table'],
-        export_data,
-        settings['colorize_title_gsheets']
-    )
+    if settings['google_sheets_write']:
+        gsheets.gsheets_save(
+            settings['gsheets_data_file'],
+            settings['table'],
+            export_data,
+            settings['colorize_title_gsheets']
+        )
